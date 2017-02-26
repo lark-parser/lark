abfa14c721420d2638c48f080be0811bf24fabc8
Erez Shinan <erezshin+git@gmail.com>
2017-02-26 12:07:29 +0200

    Tiny fix in reconstruct_json (Py3)

--- examples/reconstruct_json.py
+++ examples/reconstruct_json.py
@@ -27,14 +27,14 @@ def test():
     json_parser = Lark(json_grammar)
     tree = json_parser.parse(test_json)
 
-    # print '@@', tree.pretty()
+    # print ('@@', tree.pretty())
     # for x in tree.find_data('true'):
     #     x.data = 'false'
     #     # x.children[0].value = '"HAHA"'
 
 
     new_json = Reconstructor(json_parser).reconstruct(tree)
-    print new_json
-    print json.loads(new_json) == json.loads(test_json)
+    print (new_json)
+    print (json.loads(new_json) == json.loads(test_json))
 
 test()

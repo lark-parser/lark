7f8af5820049bfc96d7ca27b74f7a77d705f6a59
Erez Shinan <erezshin+git@gmail.com>
2018-08-05 15:36:06 +0300

    Fix for issue #188

--- lark/load_grammar.py
+++ lark/load_grammar.py
@@ -605,8 +605,8 @@ class GrammarLoader:
             tree = self.canonize_tree.transform( self.parser.parse(grammar_text+'\n') )
         except UnexpectedCharacters as e:
             context = e.get_context(grammar_text)
-            raise GrammarError("Unexpected input %r at line %d column %d in %s" %
-                               (context, e.line, e.column, grammar_name))
+            raise GrammarError("Unexpected input at line %d column %d in %s: \n\n%s" %
+                               (e.line, e.column, grammar_name, context))
         except UnexpectedToken as e:
             context = e.get_context(grammar_text)
             error = e.match_examples(self.parser.parse, {

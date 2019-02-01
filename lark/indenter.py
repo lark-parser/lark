12a0cbae0c7561aa1e79c0668216353ad7176cfc
Erez Shinan <erezshin+git@gmail.com>
2019-02-01 12:21:34 +0200

    Correction for commit b83bac4 (Issue #316)

--- lark/indenter.py
+++ lark/indenter.py
@@ -5,8 +5,9 @@ from .lexer import Token
 ###{standalone
 class Indenter:
     def __init__(self):
-        self.paren_level = 0
-        self.indent_level = [0]
+        self.paren_level = None
+        self.indent_level = None
+        assert self.tab_len > 0
 
     def handle_NL(self, token):
         if self.paren_level > 0:
@@ -28,8 +29,6 @@ class Indenter:
             assert indent == self.indent_level[-1], '%s != %s' % (indent, self.indent_level[-1])
 
     def _process(self, stream):
-        self.paren_level = 0
-        self.indent_level = [0]
         for token in stream:
             if token.type == self.NL_type:
                 for t in self.handle_NL(token):

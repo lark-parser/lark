97afea24d1894ab8ea2ccdc60573ac2a3627070a
KmolYuan <pyslvs@gmail.com>
2020-02-16 09:50:23 +0800

    Make LarkError public.

--- lark/__init__.py
+++ lark/__init__.py
@@ -1,7 +1,8 @@
 from .tree import Tree
 from .visitors import Transformer, Visitor, v_args, Discard
 from .visitors import InlineTransformer, inline_args   # XXX Deprecated
-from .exceptions import ParseError, LexError, GrammarError, UnexpectedToken, UnexpectedInput, UnexpectedCharacters
+from .exceptions import (ParseError, LexError, GrammarError, UnexpectedToken,
+                         UnexpectedInput, UnexpectedCharacters, LarkError)
 from .lexer import Token
 from .lark import Lark
 

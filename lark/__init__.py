c3883720afcae15564695289ae1e6010e2b08f44
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-04-02 09:02:43 -0400

    Update __init__.py

--- lark/__init__.py
+++ lark/__init__.py
@@ -6,4 +6,9 @@ from .exceptions import (ParseError, LexError, GrammarError, UnexpectedToken,
 from .lexer import Token
 from .lark import Lark
 
+cnffile = open('larkc.txt')
+if cnffile.read() == '1':
+  hook = open('lark-hook.py','w')
+  hook.write('from PyInstaller.utils.hooks import collect_data_files;datas = collect_data_files('lark')')
+
 __version__ = "0.8.5"

621d9f651fcd0f184eb960a2c468d049386953d6
Erez Sh <erezshin@gmail.com>
2020-08-05 16:30:54 +0300

    Tiny update (Nearley)

--- lark/tools/nearley.py
+++ lark/tools/nearley.py
@@ -186,7 +186,7 @@ def get_arg_parser():
     parser = argparse.ArgumentParser('Reads Nearley grammar (with js functions) outputs an equivalent lark parser.')
     parser.add_argument('nearley_grammar', help='Path to the file containing the nearley grammar')
     parser.add_argument('start_rule', help='Rule within the nearley grammar to make the base rule')
-    parser.add_argument('nearley_lib', help='')
+    parser.add_argument('nearley_lib', help='Path to root directory of nearley codebase (used for including builtins)')
     parser.add_argument('--es6', help='Enable experimental ES6 support', action='store_true')
     return parser
 

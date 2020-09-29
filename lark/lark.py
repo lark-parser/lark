aac21d4d19755c57e8cabe175663ab2020b73e21
Erez Sh <erezshin@gmail.com>
2020-09-29 14:41:54 +0300

    Minor version bump + Adjustments to PR

--- lark/lark.py
+++ lark/lark.py
@@ -336,8 +336,7 @@ class Lark(Serialize):
         Useful for caching and multiprocessing.
         """
         data, m = self.memo_serialize([TerminalDef, Rule])
-        pickle.dump({'data': data, 'memo': m}, f,
-                    protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump({'data': data, 'memo': m}, f, protocol=pickle.HIGHEST_PROTOCOL)
 
     @classmethod
     def load(cls, f):

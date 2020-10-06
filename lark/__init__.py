2dbfa2573aa9727f89670c4d90b5334e487d908b
Erez Sh <erezshin@gmail.com>
2020-10-06 23:19:44 +0300

    Added non-recursive transformer to default imports

--- lark/__init__.py
+++ lark/__init__.py
@@ -1,6 +1,6 @@
 from .utils import logger
 from .tree import Tree
-from .visitors import Transformer, Visitor, v_args, Discard
+from .visitors import Transformer, Visitor, v_args, Discard, Transformer_NonRecursive
 from .visitors import InlineTransformer, inline_args   # XXX Deprecated
 from .exceptions import (ParseError, LexError, GrammarError, UnexpectedToken,
                          UnexpectedInput, UnexpectedCharacters, LarkError)

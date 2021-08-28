ca1131a3a19ce2817dc95131333e81d4d20b168c
Erez Sh <erezshin@gmail.com>
2021-08-28 11:13:01 +0100

    Examples: Moved example into 'composition' folder, and improved it

---
+++ examples/composition/eval_json.py
@@ -0,0 +1,15 @@
+from lark import Transformer, v_args
+
+class JsonTreeToJson(Transformer):
+    @v_args(inline=True)
+    def string(self, s):
+        return s[1:-1].replace('\\"', '"')
+
+    array = list
+    pair = tuple
+    object = dict
+    number = v_args(inline=True)(float)
+
+    null = lambda self, _: None
+    true = lambda self, _: True
+    false = lambda self, _: False

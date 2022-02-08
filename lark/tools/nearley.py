985e7222ab03f0f55ce01c3b5dab50fea6b5ff9e
konstantin <konstantin.klein@hochfrequenz.de>
2022-02-08 19:31:34 +0100

    PEP 8: E225 missing whitespace around operator

--- lark/tools/nearley.py
+++ lark/tools/nearley.py
@@ -195,7 +195,7 @@ def get_arg_parser():
 
 if __name__ == '__main__':
     parser = get_arg_parser()
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     args = parser.parse_args()

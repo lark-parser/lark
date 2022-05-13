c3274801b3455dbcba237ee4f2d8f0e2a8ba4c12
MegaIng <cornelius@krupp.hamburg>
2022-05-13 10:31:37 +0200

    Skip test_nearley.py when js2py  doesn't recognize the python version.

--- tests/test_nearley/test_nearley.py
+++ tests/test_nearley/test_nearley.py
@@ -19,7 +19,12 @@ if not os.path.exists(BUILTIN_PATH):
     logger.warning("Nearley not included. Skipping Nearley tests! (use git submodule to add)")
     raise ImportError("Skipping Nearley tests!")
 
-import js2py    # Ensures that js2py exists, to avoid failing tests
+try:
+    import js2py    # Ensures that js2py exists, to avoid failing tests
+except RuntimeError as e:
+    if "python version" in str(e):
+        raise ImportError("js2py does not support this python version")
+    raise
 
 
 class TestNearley(unittest.TestCase):

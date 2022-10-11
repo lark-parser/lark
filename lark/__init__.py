89a480daeb27b81c5874f96126d014e263b88e9f
Erez Shinan <erezshin+git@gmail.com>
2022-10-11 10:14:29 +0200

    Minor version bump (1.1.3)

--- lark/__init__.py
+++ lark/__init__.py
@@ -6,4 +6,4 @@ from .exceptions import (ParseError, LexError, GrammarError, UnexpectedToken,
 from .lexer import Token
 from .lark import Lark
 
-__version__: str = "1.1.2"
+__version__: str = "1.1.3"

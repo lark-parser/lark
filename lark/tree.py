652b92c17508e81412b6f968b28f221f9aa1908a
Jitendra Mishra <jitendramishra.jmishra@gmail.com>
2022-12-05 16:57:16 +0530

    Use f-string

--- lark/tree.py
+++ lark/tree.py
@@ -68,17 +68,16 @@ class Tree(Generic[_Leaf_T]):
         return self.data
 
     def _pretty(self, level, indent_str):
-        yield indent_str*level
-        yield self._pretty_label()
+        yield f'{indent_str*level}{self._pretty_label()}'
         if len(self.children) == 1 and not isinstance(self.children[0], Tree):
-            yield from (i for i in ('\t', '%s' % (self.children[0],), '\n'))
+            yield f'\t{self.children[0]}\n'
         else:
             yield '\n'
             for n in self.children:
                 if isinstance(n, Tree):
                     yield from n._pretty(level+1, indent_str)
                 else:
-                    yield from (i for i in (indent_str*(level+1), '%s' % (n,), '\n'))
+                    yield f'{indent_str*(level+1)}{n}\n'
 
     def pretty(self, indent_str: str='  ') -> str:
         """Returns an indented string representation of the tree.

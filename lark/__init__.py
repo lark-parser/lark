d447c4ec343fc4060a0479e49f58284bf0ffa1d2
Erez Shinan <erezshin+git@gmail.com>
2025-09-22 14:59:05 +0200

    Version bump (1.3.0)

--- lark/__init__.py
+++ lark/__init__.py
@@ -14,7 +14,7 @@ from .tree import ParseTree, Tree
 from .utils import logger, TextSlice
 from .visitors import Discard, Transformer, Transformer_NonRecursive, Visitor, v_args
 
-__version__: str = "1.2.2"
+__version__: str = "1.3.0"
 
 __all__ = (
     "GrammarError",

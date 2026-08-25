a21e6a819e0637e783db8ef0c7cfca250187fb05
Arda ÖZCAN <76063457+ardozcnn@users.noreply.github.com>
2026-08-25 22:00:55 +0300

    Update grammar.py

--- lark/grammar.py
+++ lark/grammar.py
@@ -1,8 +1,15 @@
-from typing import Any, Dict, Optional, Tuple, ClassVar, Sequence, Callable
+import sys
+from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, ClassVar, Sequence, Callable
 
 from .utils import Serialize
 
-###{standalone
+if TYPE_CHECKING:
+    if sys.version_info >= (3, 11):
+        from typing import Self
+    else:
+        from typing_extensions import Self
+    from .utils import SerializeMemoizer
+
 TOKEN_DEFAULT_PRIORITY = 0
 
 
@@ -33,7 +40,7 @@ class Symbol(Serialize):
     def fullrepr(self) -> str:
         return repr(self)
 
-    def renamed(self, f: Callable[[str], str]) -> 'Symbol':
+    def renamed(self, f: Callable[[str], str]) -> 'Self':
         return type(self)(f(self.name))
 
 
@@ -60,90 +67,7 @@ class NonTerminal(Symbol):
 
     is_term: ClassVar[bool] = False
 
-    def serialize(self, memo: Optional[Dict[Any, Any]] = None) -> Dict[str, Any]:
-        # TODO: this is here because self.name can be a Token instance.
-        #       remove this function when the issue is fixed. (backwards-incompatible)
+    def serialize(self, memo: Any = None) -> Dict[str, Any]:
+        # TODO this is here because self.name can be a Token instance.
+        #      remove this function when the issue is fixed. (backwards-incompatible)
         return {'name': str(self.name), '__type__': 'NonTerminal'}
-
-
-class RuleOptions(Serialize):
-    __serialize_fields__ = ('keep_all_tokens', 'expand1', 'priority', 'template_source', 'empty_indices')
-
-    keep_all_tokens: bool
-    expand1: bool
-    priority: Optional[int]
-    template_source: Optional[str]
-    empty_indices: Tuple[bool, ...]
-
-    def __init__(
-        self,
-        keep_all_tokens: bool = False,
-        expand1: bool = False,
-        priority: Optional[int] = None,
-        template_source: Optional[str] = None,
-        empty_indices: Tuple[bool, ...] = ()
-    ) -> None:
-        self.keep_all_tokens = keep_all_tokens
-        self.expand1 = expand1
-        self.priority = priority
-        self.template_source = template_source
-        self.empty_indices = empty_indices
-
-    def __repr__(self) -> str:
-        return (
-            f'RuleOptions({self.keep_all_tokens!r}, {self.expand1!r}, '
-            f'{self.priority!r}, {self.template_source!r}, {self.empty_indices!r})'
-        )
-
-
-class Rule(Serialize):
-    """
-    origin : a symbol
-    expansion : a list of symbols
-    order : index of this expansion amongst all rules of the same name
-    """
-    __slots__ = ('origin', 'expansion', 'alias', 'options', 'order', '_hash')
-
-    __serialize_fields__ = ('origin', 'expansion', 'order', 'alias', 'options')
-    __serialize_namespace__ = (Terminal, NonTerminal, RuleOptions)
-
-    origin: NonTerminal
-    expansion: Sequence[Symbol]
-    order: int
-    alias: Optional[str]
-    options: RuleOptions
-    _hash: int
-
-    def __init__(
-        self,
-        origin: NonTerminal,
-        expansion: Sequence[Symbol],
-        order: int = 0,
-        alias: Optional[str] = None,
-        options: Optional[RuleOptions] = None
-    ) -> None:
-        self.origin = origin
-        self.expansion = expansion
-        self.alias = alias
-        self.order = order
-        self.options = options or RuleOptions()
-        self._hash = hash((self.origin, tuple(self.expansion)))
-
-    def _deserialize(self) -> None:
-        self._hash = hash((self.origin, tuple(self.expansion)))
-
-    def __str__(self) -> str:
-        expansion_str = ' '.join(x.name for x in self.expansion)
-        return f'<{self.origin.name} : {expansion_str}>'
-
-    def __repr__(self) -> str:
-        return f'Rule({self.origin!r}, {self.expansion!r}, {self.alias!r}, {self.options!r})'
-
-    def __hash__(self) -> int:
-        return self._hash
-
-    def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, Rule):
-            return False
-        return self.origin == other.origin and self.expansion == other.expansion
-###}
